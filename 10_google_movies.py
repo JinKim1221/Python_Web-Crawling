7752fa846431b332781b8d401ee92bb426b1a575
JinKim1221 <sdo7329@gmail.com>
2021-06-26 22:40:06 +0100

    download first 5 movie posts for the last 5 years

--- 10_google_movies.py
+++ 10_google_movies.py
@@ -2,21 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 
 
-headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0"}
-url = "https://www.google.com/search?q=movies+2020&sxsrf=ALeKk00LWCszIvQLPLxN0IOPub6UsxyQqg%3A1624741690419&source=hp&ei=OpfXYNzHF6OL1fAP9OKwmAI&iflsig=AINFCbYAAAAAYNelSuO9CU55VXgJv7n6ZulmbhQ4QfzZ&oq=movi&gs_lcp=Cgdnd3Mtd2l6EAMYADIECCMQJzIECCMQJzIECCMQJzIHCAAQsQMQQzIHCAAQsQMQQzIECAAQQzIECAAQQzIHCAAQsQMQQzIECAAQQzIECAAQQzoECC4QQ1D4FliuHGDBImgAcAB4AIABT4gBmwKSAQE0mAEAoAEBqgEHZ3dzLXdpeg&sclient=gws-wiz"
+for year in range(2016, 2021) :
 
-res = requests.get(url, headers=headers)
-res.raise_for_status()
-soup = BeautifulSoup(res.text, "lxml")
+    headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0"}
+    url = "https://www.google.com/search?q=movies+{}&sxsrf=ALeKk00LWCszIvQLPLxN0IOPub6UsxyQqg%3A1624741690419&source=hp&ei=OpfXYNzHF6OL1fAP9OKwmAI&iflsig=AINFCbYAAAAAYNelSuO9CU55VXgJv7n6ZulmbhQ4QfzZ&oq=movi&gs_lcp=Cgdnd3Mtd2l6EAMYADIECCMQJzIECCMQJzIECCMQJzIHCAAQsQMQQzIHCAAQsQMQQzIECAAQQzIECAAQQzIHCAAQsQMQQzIECAAQQzIECAAQQzoECC4QQ1D4FliuHGDBImgAcAB4AIABT4gBmwKSAQE0mAEAoAEBqgEHZ3dzLXdpeg&sclient=gws-wiz".format(year)
 
-images = soup.find_all("div", attrs={"class" : "VbZgje"})
-# print(len(images))
-for idx, image in enumerate(images) :
-    image_url = image["data-tu"]
-    print(image_url)
-    
-    image_res = requests.get(image_url)
-    image_res.raise_for_status()
+    res = requests.get(url, headers=headers)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, "lxml")
 
-    with open("movie{}.jpg".format(idx+1), "wb") as f :
-        f.write(image_res.content) 
+    images = soup.find_all("div", attrs={"class" : "VbZgje"})
+    # print(len(images))
+    for idx, image in enumerate(images) :
+        image_url = image["data-tu"]
+        print(image_url)
+        
+        image_res = requests.get(image_url)
+        image_res.raise_for_status()
+
+        with open("movie_{}_{}.jpg".format(year, idx+1), "wb") as f :
+            f.write(image_res.content) 
+
+        if idx >= 4 : # first 5 movies
+            break
