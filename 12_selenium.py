a2268af601cad6f588f79b37962f92ab2c53112f
JinKim1221 <sdo7329@gmail.com>
2021-06-30 20:50:24 +0100

    searching and click using selenium

--- 12_selenium.py
+++ 12_selenium.py
@@ -1,20 +1,27 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Firefox() # "./geckodriver.exe"
 
 
 headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0"}
-url = "https://www.google.com/?gws_rd=ssl"
+url = "https://www.naver.com/"
 
-browser.get(url)
+# browser.get(url)
+# element = browser.find_element_by_id("query")
+# element.click()
+# element.send_keys("web crawling")
+# element.send_keys(Keys.ENTER)
 
+browser.get("http://daum.net")
+elem = browser.find_element_by_name("q")
+elem.send_keys("web crawling")
+# elem.send_keys(Keys.ENTER)
+elem = browser.find_element_by_xpath("/html/body/div[2]/header/div[1]/div/div[1]/form/fieldset/div/div/button[2]")
+# print(elem)
+elem.click()
 
-element = browser.find_element_by_id("L2AGLb")
-element.click()
-# background_elem = browser.find_element_by_class_name("o3j99 ikrT4e om7nvf")
-# background_elem.click()
-# login_elem = browser.find_element_by_class_name("gb_3 gb_4 gb_9d gb_3c")
-# login_elem.click()
+browser.quit()
 # browser.back()
 # browser.forward()
 # browser.refresh()
